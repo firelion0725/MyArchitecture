0e000c31cd143ab2c2201d4a9ca6389815a11421
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-04-15 10:21:52 +0800

    test 内增加了一些高阶函数 使用的测试

--- app/src/test/java/com/leo/architecture/test/ExampleUnitTest.kt
+++ app/src/test/java/com/leo/architecture/test/ExampleUnitTest.kt
@@ -1,17 +1,68 @@
 package com.leo.architecture.test
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
-
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * 测试了一些高阶函数写法
  */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun t() {
+//        test { paramFun2() }
+//        println("${test2("aaaaaaaaa").invoke("aaaaaaaaa")}")
+//        println("${test2("aaaaaaa243534aa").invoke("aaaaaaa243534aa")}")
+
+//        println("${test3("aaaaaaaaa").invoke("aaaaaaaaa").invoke()}")
+//        println("${test3("aaaaaaa243534aa").invoke("aaaaaaa243534aa").invoke()}")
+
+        println(test4{
+            paramFun2()
+        }.invoke( paramFun2()))
+    }
+
+    private fun test4(p: () -> Int): (Int) -> String {
+        return fun(p): String {
+            return if (p > 100) {
+                "so  big"
+            } else {
+                "so so "
+            }
+        }
+    }
+
+    private fun test2(p: String): (String) -> Int {
+        return fun(p): Int {
+            if (p == "aaaaaaaaa") {
+                return 800
+            }
+            return 35
+        }
+    }
+
+    private fun test3(p: String): (String) -> () -> Int {
+        return fun(p): () -> Int {
+            if (p == "aaaaaaaaa") {
+                return fun(): Int { return 800 }
+            }
+            return fun(): Int { return 35 }
+        }
+    }
+
+    private fun test(p: () -> Int) {
+        print("${p.invoke()}")
+    }
+
+    fun paramFun(): String {
+        return "aaaaa"
+    }
+
+    fun paramFun2(): Int {
+        return 99
     }
 }
