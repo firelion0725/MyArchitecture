8b6a35e40c494512d4d35713eb89d1e0a1a20b60
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-04-15 14:19:51 +0800

    增加一组协程 调用接口的写法

--- app/src/main/java/com/leo/architecture/test/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/leo/architecture/test/ui/home/HomeViewModel.kt
@@ -2,11 +2,14 @@ package com.leo.architecture.test.ui.home
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.leo.architecture.test.data.api.GankApiRepository
+import com.leo.architecture.test.data.model.BannerTitleModel
 import com.leo.architecture.test.ui.base.BaseViewModel
-import io.reactivex.SingleObserver
-import io.reactivex.disposables.Disposable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class HomeViewModel : BaseViewModel() {
 
@@ -17,7 +20,21 @@ class HomeViewModel : BaseViewModel() {
 //        message.set(str)
         messageLiveData.value = str
 //        Log.i("aaaaaaaaaaaa","message: $message")
-        Log.i("aaaaaaaaaaaa", "message: $messageLiveData.value")
+        Log.i("aaaaaaaaaaaa", "message: ${messageLiveData.value}")
+    }
+
+    fun getBanners2() {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val data: BannerTitleModel = GankApiRepository.getBanners().blockingGet()
+                withContext(Dispatchers.Main) {
+                    setMessageText("${data.status}")
+                }
+            } catch (e:Exception) {
+                println(e.message)
+            }
+
+        }
     }
 
     fun getBanners() {
@@ -29,8 +46,6 @@ class HomeViewModel : BaseViewModel() {
                 println(it.message)
             }))
     }
-
-
 
 
 }
