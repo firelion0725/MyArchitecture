4cd00baabf8db5c72e7a488c532351c40dc1cc41
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-04-30 09:57:46 +0800

    网络层改为多baseUrl 方式

--- data/src/main/java/com/leo/architecture/test/data/RetrofitManger.kt
+++ data/src/main/java/com/leo/architecture/test/data/RetrofitManger.kt
@@ -1,5 +1,6 @@
 package com.leo.architecture.test.data
 
+import com.leo.architecture.test.data.comm.baseUrl
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -12,13 +13,26 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 object RetrofitManger {
 
-    private const val baseUrl = "https://gank.io/api/"
+    private val retrofitMap = HashMap<String, Retrofit>()
+
+    fun getInstance(url: String): Retrofit {
+        var instance = retrofitMap[url]
+        if (instance == null) {
+            instance = create()
+            retrofitMap[url] = instance
+        }
+        return instance
+    }
 
     val instance: Retrofit = create()
 
     private fun create(): Retrofit {
+        return create(baseUrl)
+    }
+
+    private fun create(url: String): Retrofit {
         return Retrofit.Builder()
-            .baseUrl(baseUrl)
+            .baseUrl(url)
             .client(OkHttpManger.instance)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
--- data/src/main/java/com/leo/architecture/test/data/api/GankApiRepository.kt
+++ data/src/main/java/com/leo/architecture/test/data/api/GankApiRepository.kt
@@ -1,6 +1,7 @@
 package com.leo.architecture.test.data.api
 
 import com.leo.architecture.test.data.RetrofitManger
+import com.leo.architecture.test.data.comm.baseUrl
 import com.leo.architecture.test.data.comm.singleCompile
 import com.leo.architecture.test.data.model.BannerTitleModel
 import io.reactivex.Single
@@ -14,7 +15,7 @@ import io.reactivex.Single
 object GankApiRepository {
 
 
-    private val api: GankApi = RetrofitManger.instance.create(GankApi::class.java)
+    private val api: GankApi = RetrofitManger.getInstance(baseUrl).create(GankApi::class.java)
 
     fun getBanners(): Single<BannerTitleModel> {
         return api.getBanners()
