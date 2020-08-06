6e8ba15b365131825d10da755c36d64eb51525ec
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-08-06 17:31:07 +0800

    1.add rxpermission

--- app/src/main/java/com/leo/architecture/test/MyApplication.kt
+++ app/src/main/java/com/leo/architecture/test/MyApplication.kt
@@ -1,7 +1,10 @@
 package com.leo.architecture.test
 
 import android.app.Application
+import com.github.moduth.blockcanary.BlockCanary
+import com.github.moduth.blockcanary.BlockCanaryContext
 import com.leo.architecture.test.data.DataManger
+import com.squareup.leakcanary.LeakCanary
 
 /**
  * @author Leo.ZhangTJ
@@ -15,5 +18,14 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         DataManger.initData(this)
+        BlockCanary.install(this, AppBlockCanary()).start()
+        LeakCanary.install(this)
     }
+
+    class AppBlockCanary : BlockCanaryContext() {
+        override fun provideBlockThreshold(): Int {
+            return 1000
+        }
+    }
+
 }
